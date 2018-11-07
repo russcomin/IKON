776ea80a04e9d5654a1b41dba70cf4ecfa348ca6
unknown <jovanbelic234@gmail.com>
2018-11-07 12:36:42 +0100

    Leaderboard

--- views/app.js
+++ views/app.js
@@ -1,1 +1,11 @@
-ECHO is on.
+
+let home = document.getElementById('logo');
+	home.addEventListener('click',function () {
+		document.location = "match.html";
+	})	
+	
+let lb_button = document.getElementById('leaderboard');
+	lb_button.addEventListener('click',function () {
+		document.location = "leaderboard.html";
+	})
+	
---
+++ views/controllers/leaderboard.js
@@ -0,0 +1,1 @@
+function gotoClub() { document.location = "match.html" }
